f6c9a56acfe245f5703632e8745a878e636d03f8
mhiggins-vr <mhiggins@verticalrelevance.com>
2023-06-30 12:47:26 -0400

    changing names, adding microservice lambda

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # Monolith-to-Microservices-Foundation-Blueprint
 
+# Prerequisites (Monolith)
+CLI
+
+Docker
+
+Docker-Compose
+
+AWS CLI
+
+SSH?
+--or--
+Session Manager?
+# Validation of success (Monolith)
+Login to your AWS Account. Navigate to EC2 and find public IP address.
+
+
+App running as a monolith acting like on-prem which is on EC2
+<Screenshot>
+
+Zip code url works: go <link> here
+
+
 ## Zip Code Source
 ### https://github.com/millbj92/US-Zip-Codes-JSON (MIT)
 
@@ -15,7 +37,6 @@ pgAdmin4
 #### Executing Hydration
 ##### JSON -> CSV -> Postgres
 python3 data/json2csv.py
-
 python3 postgres/hydrate.py
 
 
---
+++ webapp-microservice/lambda.py
@@ -0,0 +1,29 @@
+import boto3
+import json
+
+print('Loading function')
+dynamo = boto3.client('dynamodb')
+
+def lambda_handler(event, context):
+
+    client = boto3.client('dynamodb')
+
+    print("!!! EVENT PATH:" + event['path'])
+    path = event['path']
+    zipcode = path.split('/')[2]
+    print("!!! ZIPCODE:" + zipcode)
+
+    response = client.get_item(TableName='zipcodes', Key={'zip_code':{'S':str(zipcode)}})
+    print(response)
+
+    print("Received event: " + json.dumps(event, indent=2))
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
+
+    # print("value1 = " + event['key1'])
+    # print("value2 = " + event['key2'])
+    # print("value3 = " + event['key3'])
+    # return event['key1']  # Echo back the first key value
+    #raise Exception('Something went wrong')
