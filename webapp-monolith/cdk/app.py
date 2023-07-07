abe7b9afae0857ded9339a6448bf95e1160601a4
mhiggins-vr <mhiggins@verticalrelevance.com>
2023-07-07 11:48:47 -0400

    Further support for Postgres on CDK EC2 instance stack. See comments for external access

--- webapp-monolith/cdk/app.py
+++ webapp-monolith/cdk/app.py
@@ -1,10 +1,13 @@
 import os.path
 
 import aws_cdk
-
+from aws_cdk.aws_s3_assets import Asset
+from aws_cdk.aws_s3_deployment import *
 from aws_cdk import (
     aws_ec2 as ec2,
     aws_iam as iam,
+    aws_s3 as s3,
+    aws_s3_deployment as s3deploy,
     App, Stack
 )
 
@@ -46,10 +49,53 @@ class EC2InstanceStack(Stack):
         #Flask Port
         instance.connections.allow_from_any_ipv4(ec2.Port.tcp(5000))
 
+        #Postgres Port
+        instance.connections.allow_from_any_ipv4(ec2.Port.tcp(5432))
+
+
+        # To Enable external connections such as PGAdmin for a GUI
+        # sudo vi /etc/postgresql/12/main/pg_hba.conf
+        #immediately below this line:
+        #host    all             all             127.0.0.1/32            md5
+        #added this line:
+        #host    all             all             0.0.0.0/0               md5
+
+        # sudo vi /etc/postgresql/10/main/postgresql.conf
+        # listen_addresses = '*'                  # what IP address(es) to listen on;.
+        # sudo systemctl restart postgresql.service
+
+
+
+
         #User data for Ubunutu Server 20.04
-        instance.add_user_data('sudo apt update');
-        instance.add_user_data('sudo apt install -y python3-pip unzip');
-        instance.add_user_data('pip3 install flask');
+        instance.add_user_data('sudo apt update')
+        instance.add_user_data('sudo apt install -y python3-pip unzip awscli')
+        instance.add_user_data('sudo apt install -y postgresql postgresql-contrib')
+        instance.add_user_data('pip3 install flask')
+        instance.add_user_data('pip3 install psycopg2-binary')
+        # instance.add_user_data('sudo -i -u postgres')
+
+        # sudo cat /var/log/cloud-init-output.log
+
+
+
+        # website_bucket = s3.Bucket(self, "WebsiteBucket",
+        #                            website_index_document="index.html",
+        #                            public_read_access=True
+        #                            )
+        #
+        # s3deploy.BucketDeployment(self, "DeployWebsite",
+        #                           sources=[s3deploy.Source.asset("./website-dist")],
+        #                           destination_bucket=website_bucket,
+        #                           destination_key_prefix="/"
+        #                           )
+
+        # Script in S3 as Asset
+        asset = Asset(self, "Asset", path=os.path.join(dirname, "../webapp.py"))
+        local_path = instance.user_data.add_s3_download_command(
+            bucket=asset.bucket,
+            bucket_key=asset.s3_object_key
+        )
 
 
         # instance.add_user_data('git clone git@github.com:VerticalRelevance/ApplicationObservability-Blueprint.git');
